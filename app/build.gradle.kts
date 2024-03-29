ea0785b4963817bba0ce0f28e802386c2f39e62c
rick <givemepassxd999@gmail.com>
2024-03-29 12:13:50 +0800

    chore: import library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "github.user.sdk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "github.user.sdk"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -51,7 +52,43 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    ext {
+        set("paging_version", "3.3.0-alpha04")
+        set("kotlin_version", "3.3.0-alpha04")
+        set("retrofit_version", "2.9.0")
+        set("retrofit_log_version", "4.9.3")
+        set("lifecycle_version", "2.7.0")
+        set("timber_version", "5.0.1")
+        set("glide_version", "4.16.0")
+        set("hilt_version", "2.44")
+    }
+
+    // hilt injection
+    implementation("com.google.dagger:hilt-android:${ext.get("hilt_version")}")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:${ext.get("glide_version")}")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:${ext.get("timber_version")}")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:${ext.get("retrofit_version")}")
+    implementation("com.squareup.retrofit2:converter-gson:${ext.get("retrofit_version")}")
+    implementation("com.squareup.retrofit2:retrofit-mock:${ext.get("retrofit_version")}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${ext.get("retrofit_log_version")}")
+
+    // lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${ext.get("lifecycle_version")}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${ext.get("lifecycle_version")}")
+
+    // paging
+    implementation("androidx.paging:paging-runtime-ktx:${ext.get("paging_version")}")
+
+    // architecture components
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
