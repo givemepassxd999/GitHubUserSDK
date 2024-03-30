b324f1ade30b78e9689f69cee406f7ab4c94ec7f
rick <givemepassxd999@gmail.com>
2024-03-30 20:59:11 +0800

    feat: add user detail info in user interface

--- app/src/main/java/github/user/sdk/ui/UserDetailFragment.kt
+++ app/src/main/java/github/user/sdk/ui/UserDetailFragment.kt
@@ -1,25 +1,41 @@
 package github.user.sdk.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.DialogFragment
@@ -76,20 +92,109 @@ class UserDetailFragment : DialogFragment() {
                 .fillMaxSize()
                 .background(color = colorResource(id = R.color.white))
         ) {
-            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            AsyncImage(
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .padding(start = 10.dp)
+                    .size(100.dp)
+                    .clip(CircleShape),
+                model = userDetail.avatarUrl ?: "",
+                contentDescription = null,
+            )
+            Text(text = userDetail.login ?: "", fontSize = 20.sp)
+            Text(text = userDetail.bio ?: "", fontSize = 20.sp)
+            HorizontalDivider()
+            Row {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = colorResource(id = R.color.black)
+                )
                 Column {
-                    AsyncImage(
-                        modifier = Modifier
-                            .padding(vertical = 10.dp)
-                            .padding(start = 10.dp)
-                            .size(100.dp)
-                            .clip(CircleShape),
-                        model = userDetail.avatarUrl ?: "",
-                        contentDescription = null,
-                    )
-                    Text(text = userDetail.login ?: "", fontSize = 20.sp)
-                    Text(text = userDetail.bio ?: "", fontSize = 20.sp)
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = user.login ?: "", color = Color.Black
+                        )
+                        if (user.siteAdmin == true) {
+                            Box(
+                                modifier = Modifier.background(
+                                    color = colorResource(
+                                        id = R.color.color_4953d4
+                                    ), shape = RoundedCornerShape(10.dp)
+                                )
+                            ) {
+                                Text(
+                                    text = getString(R.string.staff),
+                                    fontSize = 12.sp,
+                                    modifier = Modifier.padding(
+                                        vertical = 2.dp, horizontal = 10.dp
+                                    ),
+                                    color = Color.White
+                                )
+                            }
+                        }
+                    }
                 }
+
+            }
+            Row {
+                Icon(
+                    imageVector = Icons.Default.Place,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = colorResource(id = R.color.black)
+                )
+                Column {
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = userDetail.location ?: "", color = Color.Black
+                        )
+                    }
+                }
+
+            }
+            Row {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.link),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = colorResource(id = R.color.black)
+                )
+                Column {
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        val annotatedString = buildAnnotatedString {
+                            pushStringAnnotation(
+                                tag = "url",
+                                annotation = userDetail.blog ?: ""
+                            )
+                            withStyle(style = SpanStyle(color = colorResource(id = R.color.purple_700))) {
+                                append(userDetail.blog ?: "")
+                            }
+                        }
+                        ClickableText(
+                            text = annotatedString,
+                            onClick = {
+                                userDetail.blog?.let { url ->
+                                    if (url.isNotEmpty()) {
+                                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                                        startActivity(intent)
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+
             }
 
         }
